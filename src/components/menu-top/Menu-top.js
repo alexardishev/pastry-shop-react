782cde1ae8d1564aefdff68d45b52a3e0abe0f5e
Aleksandr <aaardyshev@yandex.ru>
2022-02-08 10:32:23 +0300

    menu part2

--- src/components/menu-top/Menu-top.js
+++ src/components/menu-top/Menu-top.js
@@ -4,12 +4,23 @@ import MenuTopLink from "../menu-top-link/Menu-top-link";
 import './menu-top.scss'
 
 class MenuTop extends Component {
+    state ={
+        hamb: false
+    }
+
+    hambOnClick = (e) => {
+        this.setState({hamb: !this.state.hamb})
+        console.dir(e.target)
+    }
+
+
     render() {
+        const {hamb} = this.state
         return (
             <>
-            <div className="menu__wrapper">
-                <MenuTopLink/>
-            </div>
+            <nav className="menu__wrapper">
+                <MenuTopLink hamb ={hamb} hambOnClick={this.hambOnClick}/>
+            </nav>
             </>
         )
     }
